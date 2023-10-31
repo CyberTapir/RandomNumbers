c3f34315e4fe66b6a5ef1baae12ada3535e48202
cybertapir <callumtb@CXB-PC>
2023-10-31 21:11:53 +1100

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,77 @@
+﻿Imports System
+
+Public Class Form1
+    Dim arrNums(100000) As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        initArray()
+        Dim s As New Stopwatch
+        s.Start()
+        method1()
+        s.Stop()
+        ListBox1.Items.Add(s.ElapsedMilliseconds / 10 & " seconds " & "m1")
+        initArray()
+        s.Reset()
+        s.Start()
+        method2()
+        s.Stop()
+        ListBox1.Items.Add(s.ElapsedMilliseconds / 10 & " seconds " & "m2")
+        initArray()
+        s.Reset()
+        s.Start()
+        method3()
+        s.Stop()
+        ListBox1.Items.Add(s.ElapsedMilliseconds / 10 & " seconds " & "m3")
+    End Sub
+
+    Private Sub initArray()
+        Randomize()
+        For i = 1 To 100000
+            arrNums(i) = i
+        Next i
+    End Sub
+
+    Private Sub method1()
+        Dim temp As Integer
+        Dim found As Boolean
+        For i = 2 To arrNums.Length
+            Do
+                temp = Int(Rnd() * 100000) + 1
+                found = False
+                For j = 1 To i - 1
+                    If temp = arrNums(j) Then
+                        found = True
+                        Exit For
+                    End If
+                Next j
+            Loop While found
+            arrNums(i) = temp
+        Next i
+    End Sub
+
+    Private Sub method2()
+        Dim rand As Integer
+        Dim temp As Integer
+        For i = 1 To 100000
+            rand = Int(Rnd() * 100000) + 1
+            temp = arrNums(i)
+            arrNums(i) = arrNums(rand)
+            arrNums(rand) = temp
+        Next i
+    End Sub
+
+    Private Sub method3()
+        Dim r As Integer
+        Dim flags(100000) As Boolean
+        For i = 1 To 100000
+            flags(i) = False
+        Next i
+        For i = 1 To 100000
+            Do
+                r = Int(Rnd() * 100000) + 1
+            Loop While flags(r)
+            flags(r) = True
+            arrNums(i) = arrNums(r)
+        Next i
+    End Sub
+End Class
